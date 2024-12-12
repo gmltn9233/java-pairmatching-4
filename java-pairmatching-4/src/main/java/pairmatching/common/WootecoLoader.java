4367f2ed948ae7255d56cb4a80c0d929904a0430
이희수 <hslee9234@naver.com>
2024-12-12 16:14:23 +0900

    feat: 초기 데이터셋 구현

---
+++ java-pairmatching-4/src/main/java/pairmatching/common/WootecoLoader.java
@@ -0,0 +1,37 @@
+package pairmatching.common;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import pairmatching.common.enums.Course;
+import pairmatching.common.enums.FilePath;
+import pairmatching.domain.Crew;
+import pairmatching.domain.Crews;
+import pairmatching.domain.Wooteco;
+
+public class WootecoLoader {
+    public Wooteco regisetWooteco(){
+        Map<Course,Crews> wooteco = new HashMap<>();
+        Crews backendCrews = registerCrews(FilePath.BACKEND);
+        Crews frontendCrews = registerCrews(FilePath.FRONTEND);
+        wooteco.put(Course.BACKEND,backendCrews);
+        wooteco.put(Course.FRONTEND,frontendCrews);
+        return new Wooteco(wooteco);
+    }
+
+    private Crews registerCrews(FilePath filePath){
+        List<String> names = FileReader.readFile(filePath);
+        List<Crew> crews = registerCrew(names);
+        return new Crews(crews);
+    }
+
+    private List<Crew> registerCrew(List<String> names){
+        List<Crew> crews = new ArrayList<>();
+        for(String name : names){
+            Crew crew = new Crew(name);
+            crews.add(crew);
+        }
+        return crews;
+    }
+}
